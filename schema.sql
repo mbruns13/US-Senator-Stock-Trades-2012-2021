5670152d2868e83bb80c95df950181fb97fed01a
tbryan2 <timbryan0315@gmail.com>
2022-07-26 13:47:56 -0500

    Added SQL create schema, renamed columns to fit POSTGRESQL formatting, added connection to senators_db database via sqlaclhemy

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE trades(
+	Senator VARCHAR,
+	Party VARCHAR,
+	Sex VARCHAR,
+	DateofTrade DATE,
+	Owner VARCHAR,
+	Ticker VARCHAR,
+	Sector VARCHAR,
+	TransactionType VARCHAR,
+	RangeofTransactionValue VARCHAR,
+	StockValueVsMarket1month DECIMAL,
+	StockValueChange1month DECIMAL,
+	MarketChange1month DECIMAL,
+	StockValueVsMarket1year DECIMAL,
+	StockValueChange1year DECIMAL,
+	MarketValueChange1year DECIMAL
+)
+
